141750779ca3ce5fdc8da8594bffac705a5c98e2
Loquen <lrjones416@gmail.com>
2018-09-12 20:57:23 +0000

    added new colorpicker functionality to user account settings

---
+++ app/javascript/components/Colorpicker.js
@@ -0,0 +1,24 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import { SketchPicker } from "react-color"
+
+class Colorpicker extends React.Component {
+  
+  construction(props){
+    super(props);
+  }
+  
+  handleChange = {}
+  
+  render(){
+    return (<React.Fragment>
+              <SketchPicker />
+            </React.Fragment>
+           );
+  }
+}
+
+Colorpicker.propTypes = {};
+
+export default Colorpicker;
